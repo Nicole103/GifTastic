fcfa8930832898b329ee175533be4edbfae5d53a
Nicole Heersink <nheersink8@gmail.com>
2019-11-03 19:52:49 -0700

    buttons are displaying but not calling gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,29 @@
 //global varables
 //Array for buttons
-var gifs = ["shark", "whale", "sea turtle", "seal", "Octopus"];
+var animal = ["shark", "whale", "sea turtle", "seal", "Octopus"];
+
+
+//function to display giphys
+//onclick function
+// $("#gif-btn").on("click", function () {
+function showGifs(){
+   
+    var animal = $(this).attr("data-name");
+    //giphy link besure to limit responses to no more then 10 (limit=10 at end of link)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    //ajax to GET giphys for each button value
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+       
+    });
+
+
+    
+};
 //function to render the buttons
 function renderButtons() {
 
@@ -8,30 +31,21 @@ function renderButtons() {
     $(".button-container").empty();
 
     //for loop to add button tag
-    for (var i = 0; i < gifs.length; i++) {
+    for (var i = 0; i < animal.length; i++) {
         var a = $("<button>");
         a.addClass("gif-btn");
-        a.attr("data-name", gifs[i]);
-        a.text(gifs[i]);
+        a.attr("data-name", animal[i]);
+        a.text(animal[i]);
         $(".button-container").append(a);
     }
 }
-
-renderButtons();
-
+$(document).on("click", "gif-btn", showGifs);
 //adding a button into the array
-
-//function to display giphys
-//onclick function
-
-//giphy link besure to limit responses to no more then 10 (limit=10 at end of link)
-
-//ajax to GET giphys for each button value
-
-//.then(function (response){}) to call the response from giphy forward
-    // for loop with if statement
-
-    //if statement include ratings in .text
+renderButtons();
+ // var results = response.data;
+        // for loop with if statement
+        // for(var i = 0; i < results.length; i++)
+//if statement include ratings in .text
 
     //add img tag to response
 
